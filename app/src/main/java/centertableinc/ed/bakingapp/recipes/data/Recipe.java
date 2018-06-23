41927c19f94200f4526ff11bc35ede38d2d10f3c
Azmi Rutkay BIYIK <rutkay.biyik@gmail.com>
2018-06-24 00:06:23 +0300

    List variable types are changed into ArrayList in Recipe interface

--- app/src/main/java/centertableinc/ed/bakingapp/recipes/data/Recipe.java
+++ app/src/main/java/centertableinc/ed/bakingapp/recipes/data/Recipe.java
@@ -2,13 +2,14 @@ package centertableinc.ed.bakingapp.recipes.data;
 
 import android.os.Parcelable;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public interface Recipe extends Parcelable {
     String getRecipeId();
     String getRecipeName();
-    List<RecipeIngredient> getIngredientList();
-    List<RecipeStep> getStepList();
+    ArrayList<RecipeIngredient> getIngredientList();
+    ArrayList<RecipeStep> getStepList();
     Integer getRecipeServings();
     String getRecipeImage();
 
